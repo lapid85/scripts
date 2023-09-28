33dea2bdc66fe98071c0ad5aa7e976e516846f61
neil <neil.lapid85@gmail.com>
2023-09-28 23:12:12 +0800

    完善处理条件

--- hyperf-skeleton/app/Command/GoTableCommand.php
+++ hyperf-skeleton/app/Command/GoTableCommand.php
@@ -101,12 +101,42 @@ class GoTableCommand extends HyperfCommand
         foreach ($rows as $row) {
             $fileContent .= "\t{$this->goField($row->field_name)} ".
             "{$this->goType($row->field_type)} ".
-            "`gorm:\"{$this->goColumn($row->field_name)}\"`". 
+            "`gorm:\"{$this->goColumn($row->field_name)}\" json:\"{$row->field_name}\"`". 
             "\t// {$this->goComment($row->field_name, $row->field_comment)}\n";
         }
 
-        $fileContent .= "\tgorm.Model\n";
+        $fileContent .= "\tgorm.Model `json:\"-\"`\n";
         $fileContent .= "}\n\n";
+
+        // GetAll
+        $fileContent .= "// GetAll 获取所有记录 参数: 连接对象/条件:map[string]interface{}/限制: [10:limit, 2:page]/排序:'id desc'\n";
+        $fileContent .= "func (ths *{$structName}) GetAll(db *gorm.DB, args ...interface{}) (interface{}, int64, error) {\n".
+            "\t var rows = []{$structName}{}\n".
+            "\t // var count int64 = 0\n".
+            "// 判断是否有查询条件\n".
+            "\t if len(args) > 0 {\n".
+            "\t\t db = db.Where(args[0])\n".
+            "\t }\n".
+            "// 判断是否有 [limit, page] \n".
+            "\t if len(args) > 1 {\n".
+            "\t\t limiter := args[1].([]int)\n".
+            "\t\t if len(limiter) > 0 {\n".
+            "\t\t\t db = db.Limit(limiter[0])\n".
+            "\t\t\t if len(limiter) > 1 {\n".
+            "\t\t\t\t db = db.Offset(limiter[0] * (limiter[1] - 1))\n".
+            "\t\t\t }\n".
+            "\t\t }\n".
+            "\t }\n".
+            "// 判断是否有排序\n".
+            "\t if len(args) > 2 {\n".
+            "\t\t db = db.Order(args[2].(string))\n".
+            "\t }\n".
+            "\t result := db.Find(&rows)\n".
+            "\t if result.Error != nil {\n".
+            "\t\t return nil, 0, result.Error\n".
+            "\t }\n".
+            "\t\t return rows, result.RowsAffected, nil\n".
+            "}\n\n";
 
         // 保存文件
         $savingPath = $this->config->get('go_table_path');
